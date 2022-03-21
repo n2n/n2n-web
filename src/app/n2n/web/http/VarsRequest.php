e77a09b13e61638b039382f0f1e75900a29e9bae
Andreas von Burg <a@von-burg.net>
2022-03-21 09:31:20 +0100

    VarsRequest made compatible with proxy requests

--- src/app/n2n/web/http/VarsRequest.php
+++ src/app/n2n/web/http/VarsRequest.php
@@ -120,15 +120,13 @@ class VarsRequest implements Request {
 		
 		$this->cmdContextPath = Path::create($scriptDirName, true)->chEndingDelimiter(true);
 		$this->cmdPath = Path::create(mb_substr($requestUrl, mb_strlen($scriptDirName)), true);
-		
-		$protocol = isset($this->serverVars['HTTPS']) && $this->serverVars['HTTPS'] != 'off' 
-				&& $this->serverVars['HTTPS'] ? Request::PROTOCOL_HTTPS : Request::PROTOCOL_HTTP;
-		$hostName = null;
-		if (isset($this->serverVars['HTTP_HOST'])) {
-			$hostName = $this->serverVars['HTTP_HOST'];
-		} else {
-			$hostName = $this->extractServerVar('SERVER_NAME');	
-		}
+
+		$protocol = $this->serverVars['HTTP_X_FORWARDED_PROTO'] ?? $this->serverVars['REQUEST_SCHEME']
+				?? (isset($this->serverVars['HTTPS']) && $this->serverVars['HTTPS'] != 'off'
+						&& $this->serverVars['HTTPS'] ? Request::PROTOCOL_HTTPS : Request::PROTOCOL_HTTP);
+
+		$hostName = $this->serverVars['HTTP_X_FORWARDED_HOST'] ?? $this->serverVars['HTTP_HOST']
+				?? $this->extractServerVar('SERVER_NAME');
 		
 		$this->origMethodName = mb_strtoupper($this->extractServerVar('REQUEST_METHOD'));
 		try {
