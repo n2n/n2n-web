68e4783810af35637cee918e627120054b0ad720
andreas <a@von-burg.net>
2024-09-03 15:52:45 +0200

    n2n compatibility

--- src/test/n2n/web/ext/WebN2nExtensionTest.php
+++ src/test/n2n/web/ext/WebN2nExtensionTest.php
@@ -4,15 +4,10 @@ namespace n2n\web\ext;
 
 use PHPUnit\Framework\TestCase;
 use n2n\core\N2N;
-use n2n\core\config\AppConfig;
-use n2n\util\type\ArgUtils;
 use n2n\core\config\build\CombinedConfigSource;
 use n2n\config\source\impl\IniFileConfigSource;
 use n2n\core\config\build\AppConfigFactory;
 use n2n\util\io\fs\FsPath;
-use n2n\core\cache\impl\NullAppCache;
-use n2n\core\container\impl\AppN2nContext;
-use n2n\core\container\TransactionManager;
 use n2n\core\module\ModuleManager;
 use n2n\core\VarStore;
 use n2n\core\container\impl\PhpVars;
@@ -51,10 +46,8 @@ class WebN2nExtensionTest extends TestCase {
 
 		$appN2nContext->setLookupManager(new LookupManager(new SimpleLookupSession(), new EphemeralCacheStore(), $appN2nContext));
 
-
 		$this->expectException(TypeNotFoundException::class);
 		$appN2nContext->getHttp()->invokerControllers(false);
-
 	}
 
 }
